2f00b5010a32be0964f3b9269bf6c9475cfef50e
Problem <pboyd04@gmail.com>
2015-07-13 21:38:40 +0000

    Use secure framework plugin system

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+class FVSPlugin extends SecurePlugin
+{
+    function get_secure_menu_entries($page, $user)
+    {
+        $ret = array('Volunteer'=>$page->secure_root.'fvs/index.php');
+        return $ret;
+    }
+
+    function get_plugin_entry_point()
+    {
+        return array('name'=>'Volunteer', 'link'=>'fvs/index.php');
+    }
+}
+?>
