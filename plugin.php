b9e0accba6cffed1396079e33124d2105f9ae97d
Russell <russell.t.sherman@gmail.com>
2017-02-18 10:09:12 -0600

    soften links

--- plugin.php
+++ plugin.php
@@ -3,13 +3,18 @@ class FVSPlugin extends SecurePlugin
 {
     function get_secure_menu_entries($page, $user)
     {
-        $ret = array('Volunteer'=>$page->secure_root.'fvs/index.php');
+        $ret = array(
+          'Volunteer' => $page->secureUrl.'fvs/index.php'
+        );
         return $ret;
     }
 
     function get_plugin_entry_point()
     {
-        return array('name'=>'Volunteer', 'link'=>'fvs/index.php');
+        return array(
+          'name'=>'Volunteer',
+          'link' => $page->secureUrl.'fvs/index.php'
+        );
     }
 }
 ?>
