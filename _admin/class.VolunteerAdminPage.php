5035d597cbfd5ddfe21f906eabb6d744f1f3e08b
Problem <pboyd04@gmail.com>
2019-06-14 19:11:50 +0000

    Add initial shift statistics report

--- _admin/class.VolunteerAdminPage.php
+++ _admin/class.VolunteerAdminPage.php
@@ -52,6 +52,7 @@ class VolunteerAdminPage extends \Http\FlipAdminPage
         }
         $charts_menu = array(
             'Shift Schedules' => 'shift_schedules.php',
+            'Shift Stats' => 'shift_stats.php'
         );
         $this->content['header']['sidebar']['Roles'] = array('icon' => 'fa-address-card', 'url' => 'roles.php');
         $this->content['header']['sidebar']['Shifts'] = array('icon' => 'fa-tshirt', 'url' => 'shifts.php');
