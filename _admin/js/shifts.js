26150cf41bfb8cf3d0c6b754db4dc6e5b0c04e99
Problem <pboyd04@gmail.com>
2019-06-20 17:40:19 +0000

    Fix shift display for leads

--- _admin/js/shifts.js
+++ _admin/js/shifts.js
@@ -766,6 +766,9 @@ function gotDepartments(jqXHR) {
   var array = jqXHR.responseJSON;
   var accordian = $('#accordion');
   for(var i = 0; i < array.length; i++) {
+    if(array[i].isAdmin === false) {
+      continue;
+    }
     departments[array[i].departmentID] = array[i];
     accordian.append('<div class="card"><div class="card-header" id="heading'+array[i].departmentID+'"><h2 class="mb-0"><button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse'+array[i].departmentID+'" aria-expanded="true" aria-controls="collapse'+array[i].departmentID+'">'+array[i].departmentName+'</button></h2></div><div id="collapse'+array[i].departmentID+'" class="collapse show" aria-labelledby="heading'+array[i].departmentID+'" data-parent="#accordion"><div class="card-body"><div class="list-group" id="'+array[i].departmentID+'List"><a href="#'+array[i].departmentID+'" class="list-group-item list-group-item-action" onclick="return addNewShift(this);"><i class="fas fa-plus"></i> Add new shift</a><a href="#'+array[i].departmentID+'" class="list-group-item list-group-item-action" onclick="return addNewGroup(this);"><i class="far fa-plus-square"></i> Add new shift group</a></div></div></div></div>');
   }
