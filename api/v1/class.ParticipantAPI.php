fdb1fd1f4c8871faf625186c7c22d57c04562f65
Problem <pboyd04@gmail.com>
2019-07-17 19:50:26 +0000

    Add ability to download shift schedule in iCal format

--- api/v1/class.ParticipantAPI.php
+++ api/v1/class.ParticipantAPI.php
@@ -6,6 +6,12 @@ class ParticipantAPI extends VolunteerAPI
         parent::__construct('participants', 'uid');
     }
 
+    public function setup($app)
+    {
+        parent::setup($app);
+        $app->get('/me/shifts[/]', array($this, 'getMyShifts'));
+    }
+
     protected function canCreate($request)
     {
         $this->validateLoggedIn($request);
@@ -87,5 +93,38 @@ class ParticipantAPI extends VolunteerAPI
             return $response->withStatus(404);
         }
         return $response->withJson($areas[0]);
+    }
+
+    public function getMyShifts($request, $response, $args)
+    {
+        $this->validateLoggedIn($request);
+        $uid = $this->user->uid;
+        $dataTable = DataSetFactory::getDataTableByNames('fvs', 'shifts');
+        $filter = new \Data\Filter("participant eq '$uid'");
+        $shifts = $dataTable->read($filter);
+        $text = "BEGIN:VCALENDAR\r\n";
+        $text.= "VERSION:2.0\r\n";
+        $text.= "PRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\n";
+        $count = count($shifts);
+        for($i = 0; $i < $count; $i++)
+        {
+            $text.= "BEGIN:VEVENT\r\n";
+            $text.= "UID:".$this->user->mail."\r\n";
+            $d = new DateTime($shifts[$i]['startTime']);
+            $d->setTimezone(new \DateTimeZone('UTC'));
+            $text.= "DTSTAMP:".$d->format('Ymd\THis\Z')."\r\n";
+            $text.= "DTSTART:".$d->format('Ymd\THis\Z')."\r\n";
+            $d = new DateTime($shifts[$i]['endTime']);
+            $d->setTimezone(new \DateTimeZone('UTC'));
+            $text.= "DTEND:".$d->format('Ymd\THis\Z')."\r\n";
+            $text.= "SUMMARY:".$shifts[$i]['roleID'].' '.$shifts[$i]['name']."\r\n";
+            $text.= "END:VEVENT\r\n";
+        }
+        $text.= "END:VCALENDAR\r\n";
+        $response = $response->withHeader('Content-type', 'text/calendar');
+        $response = $response->withHeader('Content-Disposition', 'attachment; filename="MyShifts.ics"');
+        $body = $response->getBody();
+        $body->write($text);
+        return $response;
     }
 }
--- index.php
+++ index.php
@@ -30,6 +30,7 @@ $page->body .= '
   </div>
   <div class="w-100"></div>
   <div id="calendar"></div>
+  <a role="button" class="btn btn-primary" href="api/v1/participants/me/shifts"><i class="far fa-calendar-alt"></i> Download My Shifts</a>
 </div>';
 
 $page->printPage();
