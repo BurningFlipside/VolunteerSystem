c833e77e39542fac7b055539102bd886d3a72e6a
Problem <pboyd04@gmail.com>
2020-02-25 21:47:58 +0000

    Display the role and department names on the pending page

--- _admin/js/pending.js
+++ _admin/js/pending.js
@@ -1,30 +1,59 @@
 var deptId;
 var table;
 
-function gotDept(jqXHR) {
-  if(jqXHR.status !== 200) {
-    alert('Unable to locate department '+deptId);
-    console.log(jqXHR);
-    return;
-  }
-  $('#deptName').html(jqXHR.responseJSON.departmentName);
+function processDept(dept) {
+  $('#deptName').html(dept.departmentName);
+  return dept;
 }
 
-function gotDeptList(jqXHR) {
-  if(jqXHR.status !== 200) {
-    console.log(jqXHR);
-    return;
-  }
-  var array = jqXHR.responseJSON;
+function processDeptList(array) {
   array = array.sort(function(a, b) {
     return a.departmentName.localeCompare(b.departmentName);
   });
+  let obj = {};
   var sel = $('#deptFilter');
   for(var i = 0; i < array.length; i++) {
     if(array[i].isAdmin) {
       addOptiontoSelect(sel[0], array[i].departmentID, array[i].departmentName);
+      obj[array[i].departmentID] = array[i];
     }
   }
+  return obj;
+}
+
+function processRoles(roles) {
+  let obj = {};
+  for(var i = 0; i < roles.length; i++) {
+    obj[roles[i].short_name] = roles[i];
+  }
+  return obj;
+}
+
+function gotInitialData(results) {
+  var deptResult = results.shift();
+  var roleResult = results.shift();
+  var obj = {};
+  if(Array.isArray(deptResult.value)) {
+    obj.depts = processDeptList(deptResult.value);
+  }
+  else {
+    obj.dept = processDept(deptResult.value);
+  }
+  obj.roles = processRoles(roleResult.value);
+  table = new Tabulator("#pendingShifts", {
+    ajaxURL: tableURL,
+    columns:[
+      {formatter: upIcon, width:40, align:"center", cellClick: approve},
+      {formatter: downIcon, width:40, align:"center", cellClick: disapprove},
+      {title:"ID", field:"_id.$oid", visible: false},
+      {title:'Department', field: 'departmentID', formatter: deptName, formatterParams: obj},
+      {title:'Role', field: 'roleID', formatter: roleName, formatterParams: obj.roles},
+      {title:'Name', field: 'name'},
+      {title:'Start Date/Time', field: 'startTime', formatter: dateTimeView},
+      {title:'End Date/Time', field: 'endTime', formatter: dateTimeView},
+      {title:'Volunteer', field: 'participant'}
+    ]
+  });
 }
 
 function deptFilterChanged(e) {
@@ -50,6 +79,28 @@ function downIcon(cell, formatterParams, onRendered) {
 function dateTimeView(cell, formatterParams, onRendered) {
   var d = new Date(cell.getValue());
   return d.toString();
+}
+
+function deptName(cell, formatterParams, onRendered) {
+  let val = cell.getValue();
+  if(formatterParams.dept !== undefined) {
+    return formatterParams.dept.departmentName;
+  }
+  if(formatterParams.depts !== undefined) {
+    let dept = formatterParams.depts[val];
+    if(dept !== undefined) {
+      return dept.departmentName;
+    }
+  }
+  return val;
+}
+
+function roleName(cell, formatterParams, onRendered) {
+  let val = cell.getValue();
+  if(formatterParams[val] !== undefined) {
+    return formatterParams[val].display_name;
+  }
+  return val;
 }
 
 function getShiftIDFromCell(cell) {
@@ -91,37 +142,31 @@ function disapprove(e, cell) {
 
 function initPage() {
   deptId = getParameterByName('dept');
+  let promises = [];
   if(deptId !== null) {
-    $.ajax({
-      url: '../api/v1/departments/'+deptId,
-      complete: gotDept
-    });
+    promises.push($.ajax({
+      url: '../api/v1/departments/'+deptId
+    }));
+    promises.push($.ajax({
+      url: '../api/v1/departments/'+deptId+'/roles'
+    }));
     tableURL = '../api/v1/departments/'+deptId+'/shifts?$filter=status%20eq%20pending and needEEApproval ne true';
+    promises.push(Promise.resolve(tableURL));
     $('#grouped_with').select2({width: '100%'});
   }
   else {
+    promises.push($.ajax({
+      url: '../api/v1/departments'
+    }));
+    promises.push($.ajax({
+      url: '../api/v1/roles'
+    }));
     $('#deptName').html('All');
     tableURL = '../api/v1/shifts?$filter=status%20eq%20pending and needEEApproval ne true';
-    $.ajax({
-      url: '../api/v1/departments',
-      complete: gotDeptList
-    });
+    promises.push(Promise.resolve(tableURL));
     $('#deptFilter').change(deptFilterChanged);
   }
-  table = new Tabulator("#pendingShifts", {
-    ajaxURL: tableURL,
-    columns:[
-      {formatter: upIcon, width:40, align:"center", cellClick: approve},
-      {formatter: downIcon, width:40, align:"center", cellClick: disapprove},
-      {title:"ID", field:"_id.$oid", visible: false},
-      {title:'Department', field: 'departmentID'},
-      {title:'Role', field: 'roleID'},
-      {title:'Name', field: 'name'},
-      {title:'Start Date/Time', field: 'startTime', formatter: dateTimeView},
-      {title:'End Date/Time', field: 'endTime', formatter: dateTimeView},
-      {title:'Volunteer', field: 'participant'}
-    ]
-  });
+  Promise.allSettled(promises).then(gotInitialData);
 }
 
 $(initPage);
