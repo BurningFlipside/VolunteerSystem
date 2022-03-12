81fe11edaecd05c7735cb7c3badca0cdda519fad
Problem <pboyd04@gmail.com>
2022-03-12 18:11:48 +0000

    Fix issues in eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,36 @@
+/* global module */
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'plugins': [
-      'security'
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'plugins': [
+    'security'
+  ],
+  'extends': [
+    'plugin:security/recommended',
+    'eslint:recommended'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1
+      }
     ],
-    'extends': [
-      'plugin:security/recommended',
-      'eslint:recommended'
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    'parserOptions': {
-        'ecmaVersion': 'latest'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2,
-            {
-                'SwitchCase': 1
-            }
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [2, 'single', { 'avoidEscape': true }],
-        'semi': [
-            'error',
-            'always'
-        ], 
-        'no-useless-escape': 'off'
-    }
+    'quotes': [2, 'single', { 'avoidEscape': true }],
+    'semi': [
+      'error',
+      'always'
+    ], 
+    'no-useless-escape': 'off'
+  }
 };
