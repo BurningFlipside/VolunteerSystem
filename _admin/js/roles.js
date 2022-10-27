332bcdf2cab736c35984252ab7048a5342a510a5
Problem <pboyd04@gmail.com>
2022-10-27 16:31:00 +0000

    Add better experience for assigned people to roles

--- _admin/js/roles.js
+++ _admin/js/roles.js
@@ -52,6 +52,14 @@ function editDone(jqXHR) {
   }
 }
 
+function editDoneRefresh(jqXHR) {
+  if(jqXHR.status !== 200) {
+    console.log(jqXHR);
+    alert('Unable to edit value!');
+  }
+  location.reload();
+}
+
 function addDone(jqXHR) {
   if(jqXHR.status === 200) {
     location.reload();
@@ -228,6 +236,10 @@ function delIcon() {
   return "<i class='fa fa-trash'></i>";
 }
 
+function emailIcon() {
+  return '<i class="fa fa-users" title="Edit the restricted to email list with help from the system."></i>';
+}
+
 function deleteDone(jqXHR) {
   if(jqXHR.status !== 200) {
     console.log(jqXHR);
@@ -280,6 +292,277 @@ function delRole(e, cell) {
   });
 }
 
+function gotEmailFailure2(email, resolve) {
+  resolve({email: email, value: [], status: 2});
+}
+
+function gotEmailSuccess2(value, email, resolve) {
+  if(value.length > 0) {
+    resolve({email: email, value: value, status: 1});
+  }
+  gotEmailFailure2(email, resolve);
+}
+
+function gotEmailFailure(email, resolve) {
+  let intPromise = $.ajax({url: '../api/v1/participants?$filter=substringof(email,\''+email+'\')'});
+  intPromise.then((value) => {gotEmailSuccess2(value, email, resolve)}).catch(() => {gotEmailFailure2(email, resolve)});
+}
+
+function gotEmailSuccess(value, email, resolve) {
+  if(value.length > 0) {
+    resolve({email: email, value: value, status: 0});
+  }
+  gotEmailFailure(email, resolve);
+}
+
+function getParticipantByEmail(email) {
+  return new Promise((resolve) => {
+    let intPromise = $.ajax({url: '../api/v1/participants?$filter=email eq \''+email+'\''});
+    intPromise.then((value) => {return gotEmailSuccess(value, email, resolve)}).catch(() => {gotEmailFailure(email, resolve)});
+  });
+}
+
+function saveEmailEdits(e) {
+  let roleName = e.data.roleId;
+  let string = e.data.emailList.join(',');
+  if($('#addUser').val().length > 0) {
+    addEmailToList(e.data.emailList, $('#addUser').val());
+    string = e.data.emailList.join(',');
+  }
+  console.log(string);
+  $.ajax({
+    url: '../api/v1/roles/'+roleName,
+    method: 'PATCH',
+    data: JSON.stringify({'requirements': {'email_list': string}}),
+    contentType: 'application/json',
+    dataType: 'json',
+    complete: editDoneRefresh
+  })
+}
+
+function addEmailToList(list, email) {
+  if(list.includes(email)) {
+    return false;
+  }
+  list.push(email);
+  return true;
+}
+
+function dragEnter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  $(this).css('border', '2px solid #0B85A1');
+}
+
+function dragOver(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function processNewEmails(emails) {
+  let roleId = $('#editEmailId').val();
+  let tableData = Tabulator.prototype.findTable('#roles')[0].getData();
+  let roleData = null;
+  for(let row of tableData) {
+    if(row.short_name === roleId) {
+      roleData = row;
+      break;
+    }
+  }
+  if(roleData === null) {
+    alert('Unable to locate data! Try again!');
+    return;
+  }
+  let oldEmails = [];
+  if(roleData.requirements !== undefined && roleData.requirements.email_list !== undefined) {
+    oldEmails = roleData.requirements.email_list.split(',');
+  }
+  let duplicates = [];
+  for(let email of emails) {
+    email = email.trim();
+    if(email.length === 0) {
+      continue;
+    }
+    if(addEmailToList(oldEmails, email) === false) {
+      duplicates.push(email);
+    }
+  }
+  let alerts = [{type:'warning', 'text':'Any added emails have not been saved yet. Please click save before leaving the page.'}];
+  if(duplicates.length > 0) {
+    alert.push({type:'warning', 'text':'Duplicate emails have been automatically discarded. '+duplicates.join(',')});
+  }
+  $('#editEmail').modal('hide');
+  showDialog(oldEmails, roleId, alerts);
+}
+
+function processResults(txt) {
+  let data = Papa.parse(txt);
+  let emails = [];
+  if(data.data.length === 0) {
+    alert('Unable to parse data. Must be in text format or csv!');
+    return;
+  }
+  if(!data.data[0][0].includes('@')) {
+    //This is maybe a CSV with headers?
+    data = Papa.parse(txt, {header: true});
+    let values = [];
+    for(let field of data.meta.fields) {
+      values.push({value: field, text: field});
+    }
+    bootbox.prompt({
+      title: "This looks like a CSV file. Which column contains the email addresses?",
+      inputType: 'select',
+      inputOptions: values,
+      callback: function(result) {
+        for(let emailArr of data.data) {
+          emails.push(emailArr[`${result}`]);
+        }
+        processNewEmails(emails);
+      }
+    });
+    return;
+  } else {
+    for(let emailArr of data.data) {
+      emails.push(emailArr[0]);
+    }  
+  }
+  processNewEmails(emails);
+}
+
+function fileRead(e) {
+  processResults(e.target.result);
+}
+
+function dropIn(e) {
+  $(this).css('border', '2px dotted #0B85A1');
+  e.preventDefault();
+  let files = e.originalEvent.dataTransfer.files;
+  for(let file of files) {
+    let reader = new FileReader();
+    reader.onload = fileRead;
+    reader.readAsText(file);
+  }
+}
+
+function showDialog(emails, roleId, extraAlerts) {
+  let promises = [];
+  for(let email of emails) {
+    email = email.trim();
+    promises.push(getParticipantByEmail(email));
+  }
+  Promise.allSettled(promises).then((results) => {
+    let newEmails = [];
+    let misCasedEmail = false;
+    let duplicateEmails = false;
+    let table = $('<table style="width: 100%;">');
+    table.append('<tr><th>Email</th><th>Status</th><th>Name</th><th>Action</th></tr>');
+    for(let result of results) {
+      result = result.value;
+      let statusStr = 'Unknown Email';
+      let statusColor = 'red';
+      let name = '';
+      let row = $('<tr>');
+      let select = $('<select>');
+      let cell = $('<td>');
+      switch(result.status) {
+        case 0:
+          statusStr = 'Good';
+          statusColor = 'green';
+          name = getNameForUser(result.value[0]);
+          if(addEmailToList(newEmails, result.email) === false) {
+            duplicateEmails = true;
+          }
+          select.append('<option value="none" selected>None</option>');
+          select.append('<option value="delete">Remove User from Role</option>');
+          select.change((e) => {
+            switch(e.currentTarget.value) {
+              case 'none':
+                addEmailToList(newEmails, result.email);
+                break;
+              case 'delete':
+                let index = newEmails.indexOf(result.email);
+                newEmails.splice(index, 1);
+                console.log(newEmails);
+                break;
+            }
+          });
+          row.append('<td>'+result.email+'</td><td style="background-color:'+statusColor+';">'+statusStr+'</td><td>'+name+'</td>');
+          cell.append(select);
+          row.append(cell);
+          table.append(row);
+          break;
+        case 1:
+          misCasedEmail = true;
+          statusStr = 'Miscased';
+          statusColor = 'yellow';
+          name = getNameForUser(result.value[0]);
+          if(addEmailToList(newEmails, result.value[0].email) === false) {
+            duplicateEmails = true;
+          }
+          table.append('<tr><td>'+result.email+'</td><td style="background-color:'+statusColor+';">'+statusStr+'</td><td>'+name+'</td><td><i>Auto Fix Email Casing</i></td></tr>');
+          break;
+        default:
+          if(addEmailToList(newEmails, result.email) === false) {
+            duplicateEmails = true;
+          }
+          select.append('<option value="none" selected>Wait for user to sign up for an account</option>');
+          select.append('<option value="delete">Remove User from Role</option>');
+          select.change((e) => {
+            switch(e.currentTarget.value) {
+              case 'none':
+                addEmailToList(newEmails, result.email);
+                break;
+              case 'delete':
+                let index = newEmails.indexOf(result.email);
+                newEmails.splice(index, 1);
+                console.log(newEmails);
+                break;
+            }
+          });
+          row.append('<td>'+result.email+'</td><td style="background-color:'+statusColor+';">'+statusStr+'</td><td>'+name+'</td>');
+          cell.append(select);
+          row.append(cell);
+          table.append(row);
+          break;
+      }
+    }
+    let alerts = [{type: 'info', text: 'You can drop a file containing a list of emails into this dialog to add more users in bulk.'}, ...extraAlerts];
+    if(misCasedEmail) {
+      alerts.push({type: 'warning', text: 'You have at least one email whose case does not match the system. Hitting save will fix this.'});
+    }
+    if(duplicateEmails) {
+      alerts.push({type: 'warning', text: 'You have at least one duplicate email address. Hitting save will fix this.'});
+    }
+    let dialogOptions = {
+      id: 'editEmail',
+      title: 'Edit Restricted Email List',
+      data: {roleId: roleId, emailList: newEmails},
+      alerts: alerts,
+      inputs: [
+        {type: 'hidden', id: 'editEmailId', value: roleId},
+        {type: 'html', text: table},
+        {label: 'Add User From System', type: 'text', id: 'addUser', keyUp: searchUsers}
+      ],
+      buttons: [
+        {text: 'Save', callback: saveEmailEdits}
+      ]
+    };
+    flipDialog.dialog(dialogOptions);
+    $('.modal.show .modal-content').on('dragenter', dragEnter);
+    $('.modal.show .modal-content').on('dragover', dragOver);
+    $('.modal.show .modal-content').on('drop', dropIn);
+  });
+}
+
+function editEmails(e, cell) {
+  let data = cell.getRow().getData();
+  let emails = [];
+  if(data.requirements !== undefined && data.requirements.email_list !== undefined) {
+    emails = data.requirements.email_list.split(',');
+  }
+  showDialog(emails, data.short_name, []);
+}
+
 function initPage() {
   deptId = getParameterByName('dept');
   let tableURL = '';
@@ -323,6 +606,7 @@ function gotCerts(jqXHR) {
   }
   var cols = [
     {formatter: delIcon, width:40, hozAlign: 'center', cellClick: delRole},
+    {formatter: emailIcon, width:40, hozAlign: 'center', cellClick: editEmails},
     {title: 'ID', field: '_id.$id', visible: false},
     {title: 'Short Name', field: 'short_name', visible: false},
     {title: 'Name', field: 'display_name', editor: 'input'},
