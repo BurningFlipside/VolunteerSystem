0603e2a2fcc08698ff924b1a35eaab18c064fdf9
Problem <pboyd04@gmail.com>
2022-10-27 19:09:32 +0000

    Add ability to filter on department for early entry report

--- _admin/report_early_entry.php
+++ _admin/report_early_entry.php
@@ -4,6 +4,15 @@
 require_once('class.VolunteerAdminPage.php');
 $page = new VolunteerAdminPage('Volunteer System Admin');
 
+$deptFilter = '';
+if($page->is_admin)
+{
+    $deptFilter = '<label for="dept" class="col-sm-2 col-form-label">Department:</label>
+    <div class="col-sm-10">
+      <select name="dept" id="dept" class="form-control"></select>
+    </div>';
+}
+
 $page->body .= '
 <div class="row">
   <div class="col-lg-12">
@@ -15,6 +24,7 @@ $page->body .= '
   <div class="col-sm-10">
     <select name="event" id="event" class="form-control"></select>
   </div>
+  '.$deptFilter.'
   <label for="startTime" class="col-sm-2 col-form-label">EE Type:</label>
   <div class="col-sm-10">
     <select name="earlyLate" id="earlyLate" class="form-control">
