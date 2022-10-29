a07cd51a3ba66bc4c0806853b8fdfc9b6d8b260f
Problem <pboyd04@gmail.com>
2022-10-29 03:41:16 +0000

    More PHPMD fixes

---
+++ api/v1/class.CertificationAPI.php
@@ -0,0 +1,33 @@
+<?php
+class CertificationAPI extends VolunteerAPI
+{
+    public function __construct()
+    {
+        parent::__construct('certifications', 'certID');
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function canUpdate($request, $entity)
+    {
+        if($this->isVolunteerAdmin($request))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function canDelete($request, $entity)
+    {
+        if($this->isVolunteerAdmin($request))
+        {
+            return true;
+        }
+        return false;
+    } 
+}
+/* vim: set tabstop=4 shiftwidth=4 expandtab: */
